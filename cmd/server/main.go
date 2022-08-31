92d9b8e8bb7e8cee34ee513a87d219f1a99356f6
beginin.vladimir <beginin.vladimir@wb.ru>
2022-08-31 08:27:36 +0700

    Add listen

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodGet {
+		http.Error(w, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("<h1>Hello, World</h1>"))
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+type apiHandler struct{}
+
+func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.Handle("/api/", apiHandler{})
+	mux.HandleFunc("/update/", updateHandler)
+	mux.HandleFunc("/", homeHandler)
+	//server := &http.Server{"127.0.0.1:8080", mux}
+	http.ListenAndServe(":8080", mux)
+}
